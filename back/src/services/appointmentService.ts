60ddf0fef5591d1807705cc8372da4a81e82da63
Camilo Pineda <b.camilo.15@gmail.com>
2024-10-20 14:02:11 -0300

    refactor(services): updates appointmentService, userService

--- back/src/services/appointmentService.ts
+++ back/src/services/appointmentService.ts
@@ -1,14 +1,40 @@
 import { AppointmentRegisterDTO } from "../dtos/AppointmentDTO"
+import { Appointment, Status } from "../interfaces/AppointmentInterface"
+import { getUserByIdService } from "./userService"
 
-export const getAppointmentService = async (): Promise<void> => {}
+const appointmentList: Appointment[] = []
+let id: number = 1
 
-export const getAppointmentByIdService = async (id: string):  Promise<string> => {
-    return id
+export const getAppointmentService = async (): Promise<Appointment[]> => {
+    return appointmentList
 }
+
+export const getAppointmentByIdService = async (id: string):  Promise<Appointment> => {
+    const appointmentFound = appointmentList.find(appointment => appointment.id === parseInt(id, 10))
+    if(!appointmentFound) throw new Error(`La cita con el id ${id} no fue encontrada`)
+    else return appointmentFound
+}
+
 export const registerAppointmentService = async (appointmentData: AppointmentRegisterDTO ): Promise<AppointmentRegisterDTO> => {
-  return appointmentData
+    const userFound = await getUserByIdService(appointmentData.userId.toString())
+    if(!userFound) throw new Error(`El usuario con id: ${appointmentData.userId} no existe`)
+    const newAppointment: Appointment = {
+        id: id++,
+        date: new Date(appointmentData.date),
+        time: appointmentData.time,
+        status: Status.active,
+        userId: appointmentData.userId
+    }
+    appointmentList.push(newAppointment)
+    return newAppointment
 }
 
-export const cancelStatusAppointmentService = async(id: string): Promise<string> => {
-  return id
+export const cancelStatusAppointmentService = async(id: string): Promise<Appointment> => {
+    const appointmentFound = appointmentList.find(appointment => appointment.id === parseInt(id, 10))
+    if(!appointmentFound) throw new Error(`La cita con el id ${id} no fue encontrada`)
+    appointmentFound.status = Status.cancelled
+    return appointmentFound
 }
+
+
+
--- back/src/services/userService.ts
+++ back/src/services/userService.ts
@@ -1,14 +1,42 @@
+import { UserDTO, UserLoginDTO, UserRegisterDTO } from "../dtos/UserDTO"
+import { User } from "../interfaces/UserInterface"
+import { getCredentialService } from "./credentialService"
 
-import { UserLoginDTO, UserRegisterDTO } from "../dtos/UserDTO"
+const users: User[] = []
+let id: number = 1
 
-export const getUserService = async (): Promise<void>  => {}
-
-export const getUserByIdService = async (id: string): Promise<string> => {
-  return id
+export const getUserService = async (): Promise<UserDTO[]>  => {
+    return users.map( user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email
+        }
+    })  
 }
 
-export const registerUserService = async (user: UserRegisterDTO): Promise<UserRegisterDTO> => {
-  return user
+export const getUserByIdService = async (id: string): Promise<UserDTO> => {
+    const userFound = users.find( user => user.id === parseInt(id, 10))
+    if(!userFound) throw new Error(`El usuario con id: ${id} no fue encontrado`)
+    else return {
+				id: userFound.id,
+				name: userFound.name,
+				email: userFound.email
+    }
+}
+
+export const registerUserService = async (user: UserRegisterDTO): Promise<User> => {
+  const idCredentialUser = await getCredentialService(user.username, user.password)
+  const newUser: User = {
+      id: id++,
+      name: user.name,
+      email: user.email,
+      birthdate: new Date(user.birthdate),
+      nDni: user.DNI,
+      credentialsId: idCredentialUser
+  }
+  users.push(newUser)
+  return newUser
 }
 
 export const loginUserService = async (userCredentials: UserLoginDTO): Promise<UserLoginDTO> => {
