39e6b28d2a0814b09403dc840649ce6d70189c25
Camilo Pineda <b.camilo.15@gmail.com>
2024-10-22 20:15:34 -0300

    update(server): update server config

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,7 +1,5 @@
-import { DataSource, Repository } from "typeorm";
+import { DataSource } from "typeorm";
 import { DB_DATABASE, DB_DROP, DB_ENTITIES, DB_HOST, DB_LOGGING, DB_PASSWORD, DB_PORT, DB_SYNC, DB_TYPE, DB_USERNAME } from "./envs";
-import { User } from "../entities/User.entity";
-import { Credential } from "../entities/Credentials.entity";
 
 export const AppDataSource = new DataSource({
     type: DB_TYPE,
@@ -16,9 +14,6 @@ export const AppDataSource = new DataSource({
     dropSchema: DB_DROP,
 })
 
-
-export const UserModel: Repository<User> = AppDataSource.getRepository(User)
-export const CredentialModel: Repository<Credential> = AppDataSource.getRepository(Credential)
 
 
 
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,15 +1,30 @@
-import express, { Application } from "express"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Application, NextFunction, Request, Response } from "express"
 import morgan from "morgan"
 import cors from "cors"
 import router from "./routes/index"
+import { ErrorResponse, PostgresError } from "./interfaces/ErrorInterface"
 
 const server: Application  = express()
 
 server.use(express.json())
 server.use(morgan("dev"))
 server.use(cors())
-
 server.use(router)
+
+server.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  const error = err as PostgresError
+
+  const errorMessage: ErrorResponse = {
+    message: "Error del servidor",
+    details: err instanceof Error ? error.detail ? error.detail : err.message : "Error desconocido",
+    code: error.code,
+  };
+  
+  if(error.code === 404) res.status(404).json({ message: errorMessage.message, details: errorMessage.details })
+  else res.status(400).json(errorMessage);
+
+});
 
 
 export default server
