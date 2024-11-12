ef9247ac6a11e9f900eb5ed6ebb4f8c3cb30af59
Camilo Pineda <b.camilo.15@gmail.com>
2024-11-12 17:52:30 -0300

    Update ( turno ): updating turno component and css

--- front/vite-project/src/components/Turno/Turno.jsx
+++ front/vite-project/src/components/Turno/Turno.jsx
@@ -1,17 +1,45 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from "prop-types"
 import Styles from "./Turno.module.css"
+import { useContext } from "react"
+import { UsersContext } from "../../context/UsersContext"
+import Swal from "sweetalert2"
+
 const Turno = ({ id, date, time, status }) => {
+
+  const { cancelAppointment } = useContext(UsersContext)
+  
+  const handleCancel = async ( ) => {
+        try {
+          await cancelAppointment(id)
+          Swal.fire({
+              icon: "warning",
+              color: "red",
+              title: "Turno cancelado con exito"
+          })
+        } catch (error) {
+          Swal.fire({
+            icon: "error",
+            title: "No se pudo cancelar el turno, intentelo nuevamente"
+          })
+        }
+  }
 
   return (
       <div className={Styles.appointmentCard}>
         <div className={Styles.appointmentHeader}>
             <h3>Turno #{id}</h3>
-            <span className={status === 'Active' ? Styles.statusActive : Styles.statusInactive}>{status}</span>
+            <span className={status === 'active' ? Styles.statusActive : Styles.statusInactive}>{status}</span>
         </div>
         <div className={Styles.appointmentDetails}>
             <p><strong>Fecha:</strong> <span>{date}</span></p>
             <p><strong>Hora:</strong> <span>{time}</span></p>
         </div>
+        <button 
+          className={`${Styles.cancelButton} ${status === "cancelled" ? Styles.disable : ""}`}
+          onClick={handleCancel}
+          disabled={status === "cancelled"}
+          > Cancelar Turno </button>
       </div>
   )
 
