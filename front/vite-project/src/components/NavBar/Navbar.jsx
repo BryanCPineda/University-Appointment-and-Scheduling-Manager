416cc71b594aaf10f36df6bfc579cf590a4e681d
Camilo Pineda <b.camilo.15@gmail.com>
2024-11-12 17:52:10 -0300

    Update ( navBar )

--- front/vite-project/src/components/NavBar/Navbar.jsx
+++ front/vite-project/src/components/NavBar/Navbar.jsx
@@ -1,18 +1,21 @@
-/* eslint-disable react/prop-types */
+ 
 import { Link, useNavigate } from "react-router-dom"
 import Styles from "./Navbar.module.css"
 import Swal from "sweetalert2"
+import { useContext } from "react"
+import { UsersContext } from "../../context/UsersContext"
 
-function Navbar({setIsLogged}){
+function Navbar(){
+
+  const { logOutUser } = useContext(UsersContext)
 
   const navigate = useNavigate()
   const handleLogOut = () =>{
+    logOutUser()
     Swal.fire({
       icon: "warning",
       title: "Tu sesion fue cerrada correctamente"
     })
-    localStorage.clear()
-    setIsLogged(false)
     navigate("/login")
   }
 
@@ -30,6 +33,11 @@ function Navbar({setIsLogged}){
           <li className={Styles.navItem}>
             <Link to="/misturnos" className={`${Styles.navLink} ${location.pathname === "/misturnos" ? Styles.active : ""} `}>
               Mis Turnos
+            </Link>
+          </li>
+          <li className={Styles.navItem}>
+            <Link to="/agendarturno" className={`${Styles.navLink} ${location.pathname === "/agendarturno" ? Styles.active : ""} `}>
+              Agendar Turno
             </Link>
           </li>
           <li className={`${Styles.navItem} ${Styles.navLink} `} onClick={handleLogOut}>
